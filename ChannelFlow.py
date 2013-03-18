4c9dbefc7aa90714c3e20d4de22636c0d1590b49
Truman Ellis <ellis.truman@gmail.com>
2013-03-18 13:53:46 -0500

    Removed scalings.

--- ChannelFlow.py
+++ ChannelFlow.py
@@ -22,10 +22,11 @@ uwp = prof[:,11]
 vwp = prof[:,12]
 
 epsilon = -bal[:,2]
+production = bal[:,3]
 p_diff = bal[:,5]
 t_diff = bal[:,6]
 
-k = 0.5*(up*up+vp*vp+wp*wp)*u_tau**2
+k = 0.5*(up*up+vp*vp+wp*wp)
 
 N = size(yp)
 
@@ -33,42 +34,39 @@ N = size(yp)
 # plot(yp, log(yp)/kappa+5)
 # plot(yp, Up)
 
-dUdyp = zeros(N)
-dkdyp = zeros(N)
-for i in range(0,N):
-  if i > 0 and i < N-1:
-    dym = yp[i+1]-yp[i]
-    dyp = yp[i]-yp[i-1]
-    dUdyp[i] = Up[i+1]*dym/((dym+dyp)*dyp) \
-        + Up[i]*(dyp-dym)/(dyp*dym) \
-        - Up[i-1]*dyp/((dym+dyp)*dym)
-    dkdyp[i] = k[i+1]*dym/((dym+dyp)*dyp) \
-        + k[i]*(dyp-dym)/(dyp*dym) \
-        - k[i-1]*dyp/((dym+dyp)*dym)
-  elif i > 0:
-    dy = yp[i]-yp[i-1]
-    dUdyp[i] = (Up[i]-Up[i-1])/dy
-    dkdyp[i] = (k[i]-k[i-1])/dy
-  elif i < N-1:
-    dy = yp[i+1]-yp[i]
-    dUdyp[i] = (Up[i+1]-Up[i])/dy
-    dkdyp[i] = (k[i+1]-k[i])/dy
+def numericalDerivative(u):
+  dudy = zeros(N)
+  for i in range(0,N):
+    if i > 0 and i < N-1:
+      dym = yp[i+1]-yp[i]
+      dyp = yp[i]-yp[i-1]
+      dudy[i] = u[i+1]*dym/((dym+dyp)*dyp) \
+          + u[i]*(dyp-dym)/(dyp*dym) \
+          - u[i-1]*dyp/((dym+dyp)*dym)
+    elif i > 0:
+      dy = yp[i]-yp[i-1]
+      dudy[i] = (u[i]-u[i-1])/dy
+    elif i < N-1:
+      dy = yp[i+1]-yp[i]
+      dudy[i] = (u[i+1]-u[i])/dy
+    
+  return dudy
 
-dUdy = u_tau*Re/delta*dUdyp
-dkdy = Re/delta*dkdyp
-l_m = kappa*delta*yt
-l_exact = sqrt(abs(uvp*u_tau**2/dUdy**2))
+dUdy = numericalDerivative(Up)
+dkdy = numericalDerivative(k)
+l_m = kappa*delta*yp
+l_exact = sqrt(abs(uvp/dUdy**2))
 
 figure()
 plot(yp, l_m)
 plot(yp, l_exact)
 
-uu_exact = up*up*u_tau**2-2/3*k
-vv_exact = vp*vp*u_tau**2-2/3*k
-ww_exact = wp*wp*u_tau**2-2/3*k
-uv_exact = uvp*u_tau**2
-uw_exact = uwp*u_tau**2
-vw_exact = vwp*u_tau**2
+uu_exact = up*up-2/3*k
+vv_exact = vp*vp-2/3*k
+ww_exact = wp*wp-2/3*k
+uv_exact = uvp
+uw_exact = uwp
+vw_exact = vwp
 
 nu_t_ke = 0.09*k**2/epsilon
 uu_ke = zeros(N)
@@ -95,8 +93,13 @@ plot(yp, vw_ke, linestyle='--', label='vw', color='y')
 legend()
 
 figure()
-transport_exact = (0.5*t_diff + p_diff)*u_tau**3
-transport_ke = nu_t_ke*dkdy
+production_ke = nu_t_ke*dUdy**2
+plot(yp, production, label='exact')
+plot(yp, production_ke, label='ke')
+
+figure()
+transport_exact = -t_diff + p_diff
+transport_ke = -numericalDerivative(nu_t_ke*dkdy)
 plot(yp, transport_exact, label='exact')
 plot(yp, transport_ke, label='ke')
 legend()
